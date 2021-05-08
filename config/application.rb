586d710fcb331f437c632dcc1a77d7550661334f
Matthew Jacques <iFtw@live.co.uk>
2021-05-08 10:23:54 +0100

    Rubocop autocorrect Style/ClassAndModuleChildren

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PortfolioAnalysis
-  class Application < Rails::Application
+class PortfolioAnalysis::Application < Rails::Application
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -22,5 +21,4 @@ module PortfolioAnalysis
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-  end
 end
